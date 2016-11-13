6b0ab1f3c651d6432f0a424bd3931ea70f1774ec
erick <erick@hotmail.ca>
2016-11-12 19:02:20 -0500

    Add ColorPoller information to the LCDInfo display

--- src/team6/finalproject/LCDInfo.java
+++ src/team6/finalproject/LCDInfo.java
@@ -6,7 +6,7 @@ import lejos.hardware.lcd.TextLCD;
 /**
  * Class that manages the LCD display for the leJOS EV3 Brick. Displays odometer and sensor readings.
  * @author Andrei Ungur
- * @version 0.1
+ * @version 0.11
  */
 
 public class LCDInfo extends PausableTimerListener{
@@ -44,10 +44,14 @@ public class LCDInfo extends PausableTimerListener{
 		LCD.drawString("H: ", 0, 2);
 		LCD.drawString("Distance: ",0,3);
 		LCD.drawString("Light Sensor: ",0,4);
+		LCD.drawString("Object: ",0,5);
+		LCD.drawString("Block: ",0,6);
 		LCD.drawInt((int)(pos[0]), 3, 0);
 		LCD.drawInt((int)(pos[1]), 3, 1);
 		LCD.drawInt((int)pos[2], 3, 2);
 		LCD.drawInt((int)distance,11,3);
 		LCD.drawInt((int)lightReading,15,4);
+		LCD.drawString(String.valueOf(ColorPoller.isObject()), 8, 5);
+		LCD.drawString(String.valueOf(ColorPoller.isBlock()), 7, 6);
 	}
 }
