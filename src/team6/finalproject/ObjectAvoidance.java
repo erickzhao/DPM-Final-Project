0a2d037242155b7998f4f1f72ea39c12c6605bf2
Kael Du <agains1595@gmail.com>
2016-11-13 14:51:23 -0500

    Modify Navigation.java to enable multithreading functions

---
+++ src/team6/finalproject/ObjectAvoidance.java
@@ -0,0 +1,30 @@
+package team6.finalproject;
+
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+
+public class ObjectAvoidance {
+	private double waypointX, waypointY;
+	private Odometer odo;
+	private Navigation nav;
+	private EV3MediumRegulatedMotor usMotor;
+	
+	ObjectAvoidance(double waypointX, double waypointY, Odometer odo, EV3MediumRegulatedMotor usMotor){
+		this.waypointX = waypointX;
+		this.waypointY = waypointY;
+		this.odo = odo;
+		this.nav = new Navigation(odo);	
+		this.usMotor = usMotor;
+	}
+	
+	
+	
+	
+	// travel to the set destination (x,y)
+	public void travelTo(double x, double y){
+		nav.travelTo(x, y);
+	}
+	
+	// 
+	
+	
+}
