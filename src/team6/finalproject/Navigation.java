fd222fc591ad4a92ea32c72f0f59e2ea75553e99
erick <erick@hotmail.ca>
2016-11-13 19:49:36 -0500

    Add methods to go straight indefinitely in Nav
    goForward() and goBackward() now go straight indefinitely at the same
    speed as travelTo(x,y).

--- src/team6/finalproject/Navigation.java
+++ src/team6/finalproject/Navigation.java
@@ -155,8 +155,13 @@ public class Navigation
 	 * Makes the robot travel forward indefinitely until stopped/interrupted.
 	 */
 	public void goForward(){
-		this.leftMotor.forward();
-		this.rightMotor.forward();	
+		this.setSpeeds(FAST,FAST);
+	}
+	/**
+	 * Makes the robot travel backward indefinitely until stopped/interrupted.
+	 */
+	public void goBackward() {
+		this.setSpeeds(-FAST,-FAST);
 	}
 	
 	/**
